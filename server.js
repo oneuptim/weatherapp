6a770bea4f0c65f8b7971ce0642e10b0acd8794a
Tim Maina <tim@stancebranding.com>
2018-03-15 21:35:52 -0500

    uncommented server for heroku

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var app = express();
 // Const keyword is better to use this way so that it's not changed. User all caps for the variable when using const kw.
 const PORT = process.env.PORT || 3000;
 
-// app.use(function (req, res, next) {
-//   if (req.headers['x-forwarded-proto'] === 'http') {
-//     next();
-//   } else {
-//     res.redirect('http://' + req.hostname + req.url);
-//   }
-// });
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('src'));
 
